13c8ad0580e3d6e1c5e4bec424431327b6d7d2b3
gilangyanuar <masdeco66@gmail.com>
2025-11-20 19:37:25 +0700

    First init

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id('role_id');
+            $table->string('role_name',50);
+            $table->primary('role_id');
+            $table->unique('role_name');
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id')->primary();
+            $table->string('name',100);
+            $table->string('nipp',50)->unique();
+            $table->string('password_hash');
+            $table->foreignId('role_id')->constrained('roles', 'role_id')->cascadeOnDelete();
+            $table->boolean('is_active');
+
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};

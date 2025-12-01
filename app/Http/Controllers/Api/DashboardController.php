7df703483bac759fda83aab27d8d7f22e2266ddf
Kenyui-Lia <reizkyfirmansyah076@gmail.com>
2025-12-01 18:34:40 +0700

    tes

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function jadwal(Request $request){
-        $user = "Cobaa";
+        $user = "Cobaa1234";
 
     }
 }

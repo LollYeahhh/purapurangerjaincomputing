69a77414bc7c933b1387e392c96721366b876c83
gilangyanuar <masdeco66@gmail.com>
2025-12-01 17:42:37 +0700

    dashboard in progres

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'APILogin']);
 
 Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/dashboard/jadwal', [DashboardController::class, 'jadwal']);
     Route::post('/auth/change-password', [AuthController::class, 'APIChangePassword']);
     Route::post('/logout', [AuthController::class, 'APILogout']);
 });
